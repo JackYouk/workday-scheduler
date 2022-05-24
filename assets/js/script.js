028e8c6aea26dcbca281b664c3f2099e7c702f29
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-05-24 14:27:08 -0700

    added the add event user input (not yet functional), fixed bug so that the clock header stays at the top

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,29 @@ const root = $('#root');
 
 // Clock header -----------------------------------------------------
 
-// add event button -------------------------------------------------
+let clockHeader = $('<h1>')
+    .text(moment().format('LLLL'))
+    .addClass('text-center m-3');
+root.prepend(clockHeader);
 
-// delete event button ----------------------------------------------
+setInterval(function(){
+    clockHeader.text(moment().format('LLLL'));
+    root.prepend(clockHeader);
+}, 1000);
+
+// event list content -----------------------------------------------
+let listContainer = $('<div class="row d-flex justify-content-center">');
+root.append(listContainer);
+
+// add event --------------------------------------------------------
+let addContainer = $('<div class="input-group mb-3 col-6">');
+let addButton = $('<button class="btn btn-outline-success" type="button" id="button-addon1">')
+    .text('+');
+let addInput = $('<input type="text" class="form-control" placeholder="Add event here" aria-label="Example text with button addon" aria-describedby="button-addon1">');
+
+addContainer.append(addButton);
+addContainer.append(addInput);
+
+listContainer.append(addContainer);
 
 // event list -------------------------------------------------------
